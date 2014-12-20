c7122a12fbb4dcdc37e6efb7b0a886639ab6c1cd
Pedro <pedro_quide@hotmail.com>
2014-12-20 17:22:36 +0000

    added codebook

--- run_analysis.R
+++ run_analysis.R
@@ -1,38 +1,47 @@
 
-merge_data <- function() {
+merge_data <- function() 
+{
+    # Get column names
     column_name <- read.table("features.txt")
-    test_data <- read.table("test/X_test.txt", colClasses="numeric", col.names=column_name[,2])
     
+    # Prepare chosen_columns vector with the columns that matter (mean and standard deviation)
+    j <- 0
+    chosen_columns <- NULL
+    for (i in column_name[,2])
+    {
+        j <- j + 1
+        if(grepl("mean", i) || grepl("std", i))
+        {
+            chosen_columns[j] <- "numeric"
+        }
+        else
+        {
+            chosen_columns[j] <- "NULL"
+        }
+    }
+    
+    # get TEST data with columns that matter
+    test_data <- read.table("test/X_test.txt", colClasses=chosen_columns, col.names=column_name[,2])
+    # insert correspondent subjects to TEST rows
     test_data <- cbind(test_data, read.table("test/subject_test.txt", colClasses="numeric", col.names="subject"))
+    # insert correspondent activities to TEST rows
     test_data <- cbind(test_data, read.table("test/y_test.txt", colClasses="numeric", col.names="activity"))
-    #test_data$subjects <- read.table("test/subject_test.txt", colClasses="numeric", col.names="subject")
-    #test_data$activity <- read.table("test/y_test.txt", colClasses="numeric", col.names="activity")
-    
 
-    train_data <- read.table("train/X_train.txt", colClasses="numeric", col.names=column_name[,2])
-    
+    # get TRAIN data with columns that matter
+    train_data <- read.table("train/X_train.txt", colClasses=chosen_columns, col.names=column_name[,2])
+    # insert correspondent subjects to TRAIN rows
     train_data <- cbind(train_data, read.table("train/subject_train.txt", colClasses="numeric", col.names="subject"))
+    # insert correspondent activities to TRAIN rows
     train_data <- cbind(train_data, read.table("train/y_train.txt", colClasses="numeric", col.names="activity"))
-    #train_data$subjects <- read.table("train/subject_train.txt", colClasses="numeric", col.names="subject")
-    #train_data$activity <- read.table("train/y_train.txt", colClasses="numeric", col.names="activity")
-    
-    
+
     #if(!(names(test_data) == names(train_data)))
     #    stop
     
     set <- rbind(test_data, train_data)
     
-    # generate vector with column names for mean and std
-    columns_mean_std <- c("subject", "activity")
-    j <- 2
-    for (i in column_name[,2])
-    {
-        if(grepl("mean", i) || grepl("std", i))
-        {
-            j <- j + 1
-            columns_mean_std[j] <- gsub(")", ".", gsub("(", ".", gsub("-", ".", i)))
-        }
-    }
-        
-    set_mean_std <- set[columns_mean_std]
+    activity_name <- read.table("activity_labels.txt")
+    
+    set[["activity"]] <- activity_name[ match(set[['activity']], activity_name[[1]] ) , 2]
+    
+    set
 }
